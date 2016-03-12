61ae1b48d8ef163c429b9f37b7d2e66e1ebdd055
Heinrich Hartmann <heinrich@heinrichhartmann.com>
2016-03-12 18:22:08 +0100

    Remove comments
    don't explain lua syntax

--- Primes.lua
+++ Primes.lua
@@ -6,15 +6,15 @@
 --     A.S. Tanenbaum - General-Purose Macro Processor as a Poor Man's Compiler-Compiler, IEEE TOSE, Sol.SE-2, No.2, JUNE 1976
 --
 
-function PRIME(N)  -- PROCEDURE DECLARATION;
-  local X, SQUARE, I, K, LIM, PRIM -- DECLARATION OF VARIABLES;
+function PRIME(N)
+  local X, SQUARE, I, K, LIM, PRIM
   local P, V = {}, {}
-  P[1] = 2 -- ASSIGNMENT TO FIRST ELEMENT OF p;
+  P[1] = 2
   X = 1
   LIM = 1
   SQUARE = 4
-  for I = 2, N do -- LOOP. I TAKES ON 2, 3, ... N;
-    repeat -- STOPS WHEN "UNTIL" CONDITION IS TRUE;
+  for I = 2, N do
+    repeat
       X = X + 2
       if SQUARE <= X then
         V[LIM] = SQUARE
@@ -30,7 +30,7 @@ function PRIME(N)  -- PROCEDURE DECLARATION;
         PRIM = X ~= V[K]
         K = K + 1
       end
-    until PRIM -- THIS LINE CLOSES THE REPEAT
+    until PRIM
     P[I] = X
   end
   return P
