da95a80c31f3ebacf20455d7210ac9dc1b88e426
Heinrich Hartmann <heinrich@heinrichhartmann.com>
2016-03-12 19:17:16 +0100

    Added prime checker

--- Primes.lua
+++ Primes.lua
@@ -36,9 +36,21 @@ function prime(N)
   return P
 end
 
+function isPrime(n)
+	primes={}
+	if n<=0 then return false end
+	if n<=2 then return true end
+	if (n%2==0) then return false end
+	for i=3,n/2,2 do
+		if (n%i==0) then return false end
+	end
+	return true
+end
+
 function main()
   for _,p in pairs(prime(100)) do
     print(p)
+    assert(isPrime(p))
   end
 end
 main()
