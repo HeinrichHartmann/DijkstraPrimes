296b0491a1bccbf2f4f16956309feed9446bc502
Heinrich Hartmann <heinrich@heinrichhartmann.com>
2016-03-12 18:25:33 +0100

    Renamed Variables. Removed explicit declarations.

--- Primes.lua
+++ Primes.lua
@@ -6,38 +6,35 @@
 --     A.S. Tanenbaum - General-Purose Macro Processor as a Poor Man's Compiler-Compiler, IEEE TOSE, Sol.SE-2, No.2, JUNE 1976
 --
 
-function PRIME(N)
-  local X, SQUARE, I, K, LIM, PRIM
-  local P, V = {}, {}
-  P[1] = 2
-  X = 1
-  LIM = 1
-  SQUARE = 4
+function prime(N)
+  local x, x_isprime = 1, nil
+  local SQUARE, LIM = 4, 1
+  local P, V = {1}, {}
   for I = 2, N do
     repeat
-      X = X + 2
-      if SQUARE <= X then
+      x = x + 2
+      if SQUARE <= x then
         V[LIM] = SQUARE
         LIM = LIM + 1
         SQUARE = P[LIM] * P[LIM]
       end
+      x_isprime = true
       local K = 2
-      local PRIM = true
-      while PRIM and K < LIM do
-        if V[K] < X then
+      while x_isprime and K < LIM do
+        if V[K] < x then
           V[K] = V[K] + P[K]
         end
-        PRIM = X ~= V[K]
+        x_isprime = x ~= V[K]
         K = K + 1
       end
-    until PRIM
-    P[I] = X
+    until x_isprime
+    P[I] = x
   end
   return P
 end
 
 function main()
-  for _,p in pairs(PRIME(100)) do
+  for _,p in pairs(prime(100)) do
     print(p)
   end
 end
